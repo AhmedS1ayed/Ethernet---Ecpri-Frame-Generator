66c7c1dbdda8987387f96221ab4ec9984fb1ad24
AhmedS1ayed <eng.ahmed.sayed.abed@gmail.com>
2023-05-22 15:58:07 +0300

    adding ifgs to the cycle

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ def configuration() :
     config.read('config.ini')
 
     stream_duration_ms=config['PACKET_CONFIG']['STREAM_DURATION_MS']
-    ifgs = config['PACKET_CONFIG']['IFGs']
+    ifgs = config['PACKET_CONFIG']['IFGs'].split('x')[1]
     src_mac = config['PACKET_CONFIG']['SOURCE_ADDRESS'].split('x')[1]
     dst_mac = config['PACKET_CONFIG']['DESTINATION_ADDRESS'].split('x')[1]
     ether_type = config['PACKET_CONFIG']['ETHER_TYPE'].split('x')[1]
@@ -17,9 +17,14 @@ def configuration() :
     src_mac = bytes.fromhex(src_mac)
     dst_mac = bytes.fromhex(dst_mac)
     ether_type = bytes.fromhex(ether_type)
+    
+    ifgs = int(ifgs,16)
+    burst_size = int(burst_size,10)
+    
 
     return {'STREAM_DURATION_MS' : stream_duration_ms \
-            ,'IFGs' : ifgs, 'SOURCE_ADDRESS' : src_mac \
+            ,'IFGs' : ifgs \
+            , 'SOURCE_ADDRESS' : src_mac \
             , 'DESTINATION_ADDRESS' : dst_mac \
             , 'ETHER_TYPE' : ether_type \
             , 'PAYLOAD_TYPE' :payload_type \
--- generate.py
+++ generate.py
@@ -1,24 +1,48 @@
 import struct
 import random
-import crcmod
 from config import configuration
 from FCS import FCS
 
 configs = configuration()
 
+burst_size = configs['BURST_SIZE']
+ifgs = configs['IFGs']
 src_mac = configs['SOURCE_ADDRESS']
 dst_mac = configs['DESTINATION_ADDRESS']
 ether_type = configs['ETHER_TYPE']
 max_packet_size = configs['MAX_PACKET_SIZE']
+min_packet_size = 72
+
+
+
+max_data_size = max_packet_size - 26
+min_data_size = min_packet_size - 26 #46-byte
 
 with open('packets.txt', 'w') as file:
-    for i in range(10):
+    for i in range(burst_size):
         preamble = b''.join([struct.pack('B',170) for _ in range(7)])
-        sfd = b''.join([struct.pack('B',171)])
+        sop = b''.join([struct.pack('B',171)])
         eth_header = struct.pack('!6s6s2s',dst_mac, src_mac, ether_type)
-        data = b''.join([struct.pack('B', random.randint(0, 255)) for _ in range(random.randint(46,max_packet_size))])
+
+        data_size = random.randint(min_data_size,max_data_size)
+        data = b''.join([struct.pack('B', random.randint(0, 255)) for _ in range(data_size)])
         fcs = FCS(data)
         
-        packet = preamble + sfd + eth_header + data + fcs
+        packet = preamble + sop + eth_header + data + fcs
         file.write(packet.hex() + '\n')
+
+
+        no_ifgs = random.randrange(4, 20 , 4)
+        ifg_cycle = b''.join([struct.pack('B',ifgs) for _ in range(no_ifgs)])
+        file.write(ifg_cycle.hex() + '\n')
+        
     file.close()
+
+
+
+#64-byte : is the minimum size of ethernet-frame excluding preamble and sop
+#8-byte : preamble + sop 
+#min-size-of-whole-packet : 64byte(ethernet-frame) + 8byte(preamble&sop) = 72-byte
+#26-byte = 7-preamble + 1-sop + 6-dst + 6-src + 2-type + 4-fcs      <========
+#min-size-of-data : 64+8
+#max-size-of-data : max-packet-size - 26
