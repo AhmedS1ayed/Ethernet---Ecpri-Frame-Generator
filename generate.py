b0e3c6dc9ccb6d7d38322165a37def5016776176
AhmedS1ayed <eng.ahmed.sayed.abed@gmail.com>
2023-05-22 18:08:20 +0300

    edit comments

--- generate.py
+++ generate.py
@@ -17,11 +17,12 @@ stream_duration_ms \
 ,ether_type \
 ,payload_type \
 ,max_packet_size \
-,min_packet_size \
+,min_packet_size\
 ,burst_size \
 ,burst_periodicity_us = configuration()
 
 
+#calculations of data size
 max_data_size = max_packet_size - 26
 min_data_size = min_packet_size - 26 #46-byte
 
@@ -56,5 +57,5 @@ with open('packets.txt', 'w') as file:
 #8-byte : preamble + sop 
 #min-size-of-whole-packet : 64byte(ethernet-frame) + 8byte(preamble&sop) = 72-byte
 #26-byte = 7-preamble + 1-sop + 6-dst + 6-src + 2-type + 4-fcs      <========
-#min-size-of-data : 64+8
+#min-size-of-data : 64 + 8 - 26
 #max-size-of-data : max-packet-size - 26
