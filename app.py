cfaac5f7b31bd9634e9f5908c8000baf656588af
AhmedS1ayed <eng.ahmed.sayed.abed@gmail.com>
2023-05-27 04:18:52 -0700

    handle packet not exist

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ from config import config_type,set_config,set_fname
 
 
 def main(arg1 = 'config.ini', arg2 = 'packets.txt'):
+    
     if(not os.access(arg1,os.R_OK)):
         print(arg1 + ' has no read permission')
         return 0
     
-    if(not os.access(arg2,os.W_OK)):
+    if(os.path.exists(arg2) and not os.access(arg2,os.W_OK)):
         print(arg2 + ' has no write permission')
         return 0
     
