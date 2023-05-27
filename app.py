3fcbf5bad6839c39d509a1b92a7d8e5d1c70f636
AhmedS1ayed <eng.ahmed.sayed.abed@gmail.com>
2023-05-27 04:13:11 -0700

    adding permission check to config and packet files

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sys
-import configparser
+import os
 sys.path.append("./generator")
 sys.path.append("./configuration")
 from generate import generate
@@ -7,6 +7,14 @@ from config import config_type,set_config,set_fname
 
 
 def main(arg1 = 'config.ini', arg2 = 'packets.txt'):
+    if(not os.access(arg1,os.R_OK)):
+        print(arg1 + ' has no read permission')
+        return 0
+    
+    if(not os.access(arg2,os.W_OK)):
+        print(arg2 + ' has no write permission')
+        return 0
+    
     try :
         set_config(arg1)
         set_fname(arg2)
@@ -19,5 +27,3 @@ def main(arg1 = 'config.ini', arg2 = 'packets.txt'):
 if __name__ == '__main__':
     args = sys.argv[1:]
     main(*args)
-
-#.configuration/configs.ini
